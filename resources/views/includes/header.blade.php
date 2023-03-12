425e32995f246433eb7ce4bb107cae7bb3ee8c86
ahmedhabeeb73 <alnaib888@gmail.com>
2023-03-12 09:24:13 +0100

    check

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,12 +1,12 @@
 <form action="">
   @csrf
   <div class="flex flex-row p-5">
-    <div class="w-6/12">
-    <input id="address" name="address" autocomplete="off" type="text" class="p-2 2-full bg-gray-200 rounded-md" placeholder="Search"> 
+    <div class="flex-1 ">
+    <input id="address" name="address" autocomplete="off" type="text" class="p-2 w-full bg-gray-100 rounded-md" placeholder="Search"> 
   </div>
 
-  <div class="w-6/12">
-  <select name="category" id="" class="p-1 mr-5 bg-gray-200 w-full rounded-md">
+  <div class="flex-1 ">
+  <select name="category" id="" class="p-2 mr-5 bg-gray-100 w-full rounded-md">
     <option value="">Choose..</option>
   </select>
   </div>
